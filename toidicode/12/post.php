c9f5c5ec62f14d229f4b46e58fa938c8b3e0aafe
DomChuc <domchuc97@gmail.com>
2018-12-21 18:42:28 +0700

    aaa

--- toidicode/12/post.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(isset($_POST['name']) && isset($_POST['age'])) {
-        echo 'Tên: ' . $_POST['name'] . '<br>';
-        echo 'Tuổi: ' . $_POST['age'];
-    }
-?>
--- toidicode/14/function.php
+++
@@ -1,4 +0,0 @@
-<?php
-    function showMess(){
-        echo 'Nếu thấy dòng này đẹp trai';
-    }
